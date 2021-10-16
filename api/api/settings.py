bb546205612b4ed7d8410929c697c2a8d3f2f3b8
Heytor Víctor <heytorvas@gmail.com>
2021-10-16 20:14:16 -0300

    feat: add filter on querystring

--- api/api/settings.py
+++ api/api/settings.py
@@ -76,18 +76,6 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('POSTGRES_DB'),
-#         'USER': os.environ.get('POSTGRES_USER'),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('POSTGRES_HOST'),
-#         'PORT': os.environ.get('POSTGRES_PORT')
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
--- api/api/views.py
+++ api/api/views.py
@@ -9,8 +9,32 @@ class UsersAPIView(APIView):
     def get(self, request):
         page = int(request.GET.get('page', 1))
         page_size = int(request.GET.get('size', 10))
+        region = str(request.GET.get('region', '')).lower()
+        type = str(request.GET.get('type', '')).lower()
 
         data = get_updated_users()
+
+        if region != '' and type != '':
+            aux = []
+            for user in data:
+                if user['location']['region'] == region and user['type'] == type:
+                    aux.append(user)
+            data = aux
+
+        if region != '' and type == '':
+            aux = []
+            for user in data:
+                if user['location']['region'] == region:
+                    aux.append(user)
+            data = aux
+
+        if region == '' and type != '':
+            aux = []
+            for user in data:
+                if user['type'] == type:
+                    aux.append(user)
+            data = aux
+
         total = len(data)
         start = (page - 1) * page_size
         end = page * page_size
