29abff1098d344c4239dc891d2abead98003a142
Heytor Víctor <heytorvas@gmail.com>
2021-10-16 19:56:54 -0300

    feat: add pagination

--- api/api/views.py
+++ api/api/views.py
@@ -1,9 +1,23 @@
 # from django.views.generic.base import View
-from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 from api.scraper import get_updated_users
 
 class UsersAPIView(APIView):
+
     def get(self, request):
-        return Response({"users": get_updated_users()})
+        page = int(request.GET.get('page', 1))
+        page_size = int(request.GET.get('size', 10))
+
+        data = get_updated_users()
+        total = len(data)
+        start = (page - 1) * page_size
+        end = page * page_size
+
+        return Response ({
+            "pageNumber": page,
+            "pageSize": page_size,
+            "totalCount": total,
+            "users": data[start:end]
+        })
