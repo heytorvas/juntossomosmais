87d2928475cc18812df1937422679fd47b71f147
Heytor Víctor <heytorvas@gmail.com>
2021-10-17 18:08:12 -0300

    feat: production environment

--- api/api/settings.py
+++ api/api/settings.py
@@ -122,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
+STATIC_ROOT = '/vol/web/static'
+
+MEDIA_URL = '/static/media/' 
+MEDIA_ROOT = '/vol/web/media' 
